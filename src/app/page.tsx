4066651f49a62daa047ce6dd1bc3ef40f94682ca
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-09-14 18:40:04 +0100

    add main page laptop styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,29 @@ export default function Home() {
   return (
     <section className={`container ${css.main}`}>
       <h1 className={css.hero_header}>
-        HELLO,<br /> I AM <br /><span>FULLSTACK DEVELOPER</span>
+        HELLO, I AM <br />
+        <span>FULLSTACK DEVELOPER</span>
       </h1>
-        <div className={css.thumb}>
-          <Image
-            className={css.photo}
-            src="/oleksii-photo.webp"
-            width="1800"
-            height="1200"
-            alt="oleksii-photo"
-          />
+      <div className={css.thumb}>
+        <Image
+          className={css.photo}
+          src="/oleksii-photo.webp"
+          width="1800"
+          height="1200"
+          alt="oleksii-photo"
+        />
       </div>
-<ButtonContactMe/>
-        <p className={css.hero_text}>
-          I create <span>modern</span> and <span>responsive</span> web
-          applications that combine <span>functionality</span> with{" "}
-          <span>clean design</span>.
-        </p>
+
+        <div className={css.btn}>
+  <ButtonContactMe/>
+        </div>
+      <p className={css.hero_text}>
+        I create <span>modern</span> and <span>responsive</span> web
+        applications that combine <span>functionality</span> with{" "}
+        <span>clean design</span>.
+      </p>
+
+
     </section>
   );
 }
