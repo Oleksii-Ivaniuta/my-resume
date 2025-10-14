a6895606bf4871e00f01ceafdbba43cff29134d7
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-10-14 17:17:29 +0100

    add download resume

--- src/components/ButtonGetResume/ButtonGetResume.tsx
+++ src/components/ButtonGetResume/ButtonGetResume.tsx
@@ -1,19 +1,22 @@
-'use client'
-
-import { useRouter } from "next/navigation"
-import css from "./ButtonGetResume.module.css"
+"use client";
+import { useRouter } from "next/navigation";
+import css from "./ButtonGetResume.module.css";
 
 export default function ButtonContactMe() {
-    const router = useRouter();
-
-    const contactMe = async () => {
-        const goToLink = () => router.push('/contacts');
-        setTimeout(goToLink, 400);
+  const router = useRouter();
+  const downloadResume = async () => {
+    const getResume = () => {
+      window.open("/Oleksii_Ivaniuta_Fullstack_Developer.pdf", "_blank");
+      router.push('/');
     }
+    setTimeout(getResume, 500);
+  };
 
-    return (      <div className={css.link_wraper}>
-        <button onClick={contactMe} className={css.link}>
-          <span className={css.link_span}>Get resume</span>
-        </button>
-      </div>)
+  return (
+    <div className={css.link_wraper}>
+      <button onClick={downloadResume} className={css.link}>
+        <span className={css.link_span}>Get resume</span>
+      </button>
+    </div>
+  );
 }
