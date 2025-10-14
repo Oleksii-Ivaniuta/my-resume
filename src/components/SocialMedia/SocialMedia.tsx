05ec0d274ce69df14853bd5c82c3985ea5953aa2
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-10-14 15:07:10 +0100

    add new icons

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -4,22 +4,22 @@ import css from './SocialMedia.module.css';
 export function SocialMedia() {
     return (<ul className={css.sm_list}>
         <li className={css.sm_item}>
-            <Link href='https://google.com'>
-                <svg width="24" height="24" className={css.sm_icon}>
+            <Link href='https://www.linkedin.com/in/oleksii-ivaniuta/' target="_blank">
+                <svg width="32" height="32" className={css.sm_icon}>
                     <use href='/icons.svg#linkedin'/>
                 </svg>
             </Link>
         </li>
         <li className={css.sm_item}>
-            <Link href='https://google.com'>
-                <svg width="24" height="24" className={css.sm_icon}>
+            <Link href="https://t.me/oivaniuta" target="_blank">
+                <svg width="32" height="32" className={css.sm_icon}>
                     <use href='/icons.svg#telegram'/>
                 </svg>
             </Link>
         </li>
         <li className={css.sm_item}>
-            <Link href='https://google.com'>
-                <svg width="24" height="24" className={css.sm_icon}>
+            <Link href='https://www.facebook.com/oleksii.ivaniuta' target="_blank">
+                <svg width="32" height="32" className={css.sm_icon}>
                     <use href='/icons.svg#facebook'/>
                 </svg>
             </Link>
