106875bcd4bfe68276980a7e6b593d2301ac4d54
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-10-25 16:44:57 +0100

    add pagination

--- src/app/[lang]/portfolio/page.tsx
+++ src/app/[lang]/portfolio/page.tsx
@@ -5,15 +5,42 @@ import { getProjects } from "@/lib/api/api";
 import { useAuthStore } from "@/lib/store/authStore";
 import PortfolioPanel from "@/components/AdminPanels/PortfolioPanel/PortfolioPanel";
 import ProjectBox from "@/components/ProjectBox/ProjectBox";
+import Pagination from "@/components/Pagination/Pagination";
+import { useEffect, useState } from "react";
 
 export default function Portfolio() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [perPage, setPerPage] = useState<number>(6);
+  
+  useEffect(() => {
+  const handleResize = () => {
+    if (window.innerWidth >= 768) {
+      setPerPage(6);
+    } else {
+      setPerPage(4);
+    }
+  };
+
+  handleResize();
+  window.addEventListener("resize", handleResize);
+
+  return () => {
+    window.removeEventListener("resize", handleResize);
+  };
+  }, []);
+  
+    const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
   const { data, isLoading, isSuccess } = useQuery({
-    queryKey: ["Projects"],
+    queryKey: ["projects", currentPage, perPage, "desc"],
     queryFn: () =>
-      getProjects({ params: { page: 1, perPage: 6, sortOrder: "desc" } }),
+      getProjects({ params: { page: currentPage, perPage: perPage, sortOrder: "desc" } }),
     placeholderData: keepPreviousData,
     refetchOnMount: true,
   });
+
+
 
   return (
     <section className={css.portfolio}>
@@ -22,7 +49,8 @@ export default function Portfolio() {
         My <span>projects</span>
       </h2>
       {isLoading ? <div className={css.loader}></div> : <div>
-         <ul className={css.list}>
+        {isSuccess && data.totalPages > 1 && <Pagination currentPage={currentPage} onPageChange={handlePageChange} pageCount={data.totalPages}/>}
+        <ul className={css.list}>
         {isSuccess &&
           data.data.map((proj) => {
             return (
@@ -38,7 +66,8 @@ export default function Portfolio() {
               </li>
             );
           })}
-      </ul>
+        </ul>
+         {isSuccess && data.totalPages > 1 && <Pagination currentPage={currentPage} onPageChange={handlePageChange} pageCount={data.totalPages}/>}
       </div>
       }
      
